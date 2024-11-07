209cb86e4e593dd2b3e5e288deac0e81fa713841
BricRoot <my@bricawa.com>
2024-11-07 19:59:09 +0800

    改用Git Tag来备份

--- push-windows.bat
+++ push-windows.bat
@@ -1,4 +1,7 @@
 @echo off
+setlocal enabledelayedexpansion
+
+:: 设置字符编码为UTF-8，避免中文乱码
 chcp 65001
 
 :: 获取当前工作目录
@@ -10,29 +13,41 @@ set scriptDir=%~dp0
 :: 切换到脚本目录
 cd /d %scriptDir%
 
+:: 获取_config.yml中的repo和branch配置
+for /f "delims=" %%i in ('findstr /i "repo" _config.yml') do set repo=%%i
+set repo=%repo:*: =%
+for /f "delims=" %%i in ('findstr /i "branch" _config.yml') do set branch=%%i
+set branch=%branch:*: =%
+
+:: 检查repo和branch是否有效
+if "%repo%"=="" (
+    echo 无法从_config.yml获取repo配置，请检查配置
+    exit /b 1
+)
+
+if "%branch%"=="" (
+    echo 无法从_config.yml获取branch配置，请检查配置
+    exit /b 1
+)
+
 :: 检查是否存在 Git 仓库
 git rev-parse --is-inside-work-tree >nul 2>&1
 if %errorlevel% neq 0 (
     echo 脚本目录没有Git仓库，将初始化Git仓库并使用_config.yml中的仓库
     
-    :: 获取 _config.yml 中的 repo 配置
+    :: 获取_config.yml中的repo配置
     for /f "delims=" %%i in ('findstr /i "repo" _config.yml') do set repo=%%i
     
     if not defined repo (
         echo 无法从_config.yml获取仓库信息，请检查_config.yml中是否包含repo配置
         exit /b
     )
-    
-    set repo=%repo:*: =%
+
     echo 使用_config.yml中的仓库地址%repo%
     
-    :: 初始化 Git 仓库
+    :: 初始化Git仓库
     git init
     git remote add origin %repo%
-    
-    :: 获取 _config.yml 中的 branch 配置
-    for /f "delims=" %%i in ('findstr /i "branch" _config.yml') do set branch=%%i
-    set branch=%branch:*: =%
     
     :: 创建并切换到指定分支
     git checkout -b %branch%
@@ -48,7 +63,7 @@ echo 开始生成静态文件
 call hexo g
 
 cls
-if exist "gulpfile.js" (
+if exist gulpfile.js (
     echo 找到gulpfile.js，开始压缩html，js，css
     call gulp
 ) else (
@@ -57,82 +72,32 @@ if exist "gulpfile.js" (
 )
 
 cls
-echo 开始推送静态文件到远程仓库
+echo 开始部署静态文件到远程仓库
 call hexo d
 
-:: hexo程序备份远程仓库工作流
 cls
-git fetch -p
+echo 开始备份Hexo源文件到Git标签
 
-:: 获取_config.yml中的branch配置
-for /f "delims=" %%i in ('findstr /i "branch" _config.yml') do set branch=%%i
+:: 排除public文件夹的所有文件
+git add --all
 
-:: 提取branch的分支名称（去除多余的空格和冒号）
-set branch=%branch:*: =%
-echo 开始检查仓库是否超过7个分支（除了%branch%）
-set count=0
+:: 删除public文件夹，不将其推送
+git reset public
 
-:: 计算远程分支数量，排除配置的branch
-for /f "delims=" %%b in ('git branch -r --sort=committerdate ^| findstr /v "%branch%"') do (
-    set /a count+=1
-)
+:: 提交Hexo源文件
+git commit -m "Backup Hexo Files"
 
-:: 获取所有远程分支，排除配置的branch
-setlocal enabledelayedexpansion
-set count=0
-set branches= 
-for /f "delims=" %%b in ('git branch -r --sort=committerdate ^| findstr /v "%branch%"') do (
-    set /a count+=1
-    set branch=%%b
-    set branch=!branch:origin/=!
-    set branches=!branches!!branch! 
-)
-
-:: 如果远程分支超过6个，则删除最久远的分支，保留6个（除了配置的branch）
-if %count% gtr 6 (
-    echo 远程备份分支超过7个，删除最久远的分支
-
-    :: 获取所有远程分支（按时间排序），排除配置的branch
-    for /f "delims=" %%b in ('git branch -r --sort=committerdate ^| findstr /v "%branch%"') do (
-        set branch=%%b
-        set branch=!branch:origin/=!
-        if /i not "!branch!"=="%branch%" (
-            echo 即将删除远程备份分支: !branch!
-            git push origin --delete !branch!
-            echo 远程备份分支!branch!已被删除
-
-            :: 删除本地分支
-            echo 即将删除本地备份分支: !branch!
-            git branch -D !branch!
-            echo 本地备份分支!branch!已被删除
-             
-            :: 只保留6个分支，删除超过的
-            set /a count-=1
-            if !count! leq 6 (
-                goto :doneDeleting
-            )
-        )
-    )
-)
-
-:doneDeleting
-endlocal
-
-:: 获取当前系统时间
+:: 获取当前系统时间，作为标签名称
 for /f "tokens=2 delims==" %%a in ('wmic path win32_operatingsystem get LocalDateTime /value') do (set t=%%a)
 set timestamp=%t:~0,4%%t:~4,2%%t:~6,2%%t:~8,2%%t:~10,2%
 
-:: 创建一个新的分支，用于推送 Hexo 源代码
-git checkout -b %timestamp%
+:: 创建一个新的标签，用于备份Hexo源文件
+git tag %timestamp%
 
-:: 提交源代码更改，没有提交信息
-git add .
-git commit -m "new files"
-
-:: 推送到 GitHub 仓库
+:: 推送标签到远程仓库
 git push origin %timestamp%
 
-echo 已经完成部署和备份
+echo 已完成Hexo源文件备份和部署
 
 :: 切换回原本目录
 cd /d %currentDir%
